5b7a0c7d8fbe51c821cb7b39c28acf8b28b6fef7
Zheronimus <35385766+Zheronimus@users.noreply.github.com>
2022-06-20 04:06:35 -0500

    Update PossibleWords.java

--- Wordle Cheat/src/PossibleWords.java
+++ Wordle Cheat/src/PossibleWords.java
@@ -13,27 +13,51 @@ public class PossibleWords extends ArrayList<String> {
     public void trimList(UserGuess userGuess) {
 
         for (String word : wordsList) {
-            for (int j = 0; j < word.length(); j++) {
-                if (Character.toString(word.charAt(j)).equalsIgnoreCase(Character.toString(userGuess.getUserGuess().charAt(j)))) {
-                    if (userGuess.getGuessRes()[j] != 1) {
+            for (int i = 0; i < word.length(); i++) {
+                if (Character.toString(word.charAt(i)).equalsIgnoreCase(Character.toString(userGuess.getUserGuess().charAt(i)))) {
+                    if (userGuess.getGuessRes()[i] != 1) {
                         this.remove(word);
                         break;
                     }
                 } else {
-                    if (userGuess.getGuessRes()[j] == 1) {
+                    if (userGuess.getGuessRes()[i] != 1) {
+                        if (userGuess.getGuessRes()[i] == 2) {
+                            if (!word.contains(Character.toString(userGuess.getUserGuess().charAt(i)).toUpperCase())) {
+                                this.remove(word);
+                                break;
+                            }
+                        }
+
+                        else if (userGuess.getGuessRes()[i] == 0 && !hasDuplicateChars(word, userGuess)) {
+                            if (word.contains(Character.toString(userGuess.getUserGuess().charAt(i)).toUpperCase())) {
+                                this.remove(word);
+                                break;
+                            }
+                        }
+                    } else {
                         this.remove(word);
                         break;
                     }
 
-                    if (!word.contains(Character.toString(userGuess.getUserGuess().charAt(j)))) {
-                        if (userGuess.getGuessRes()[j] == 2) {
-                            this.remove(word);
-                            break;
-                        }
+                    // NEED TO CHECK IF GUESS CONTAINS DUPLICATE AND ONE OF THE CHARACTER IS A "1" AND ONE IS A "2"!!!
+                }
+            }
+        }
+    }
+
+    private boolean hasDuplicateChars(String word, UserGuess userGuess) {
+
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i + 1; j < word.length(); j++) {
+                if (Character.toString(userGuess.getUserGuess().charAt(i)).equalsIgnoreCase(Character.toString(word.charAt(j)))) {
+                    if (userGuess.getGuessRes()[i] != 0 || userGuess.getGuessRes()[j] != 0) {
+                        return true;
                     }
                 }
             }
         }
+
+        return false;
     }
 
     public void displayPossibleWords() {
