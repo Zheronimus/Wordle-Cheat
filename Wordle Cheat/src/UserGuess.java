bf3930a6d81beb8308d5bb7c380a7c168ceb7d05
Zheronimus <35385766+Zheronimus@users.noreply.github.com>
2022-07-14 00:50:39 -0500

    Update UserGuess.java

--- Wordle Cheat/src/UserGuess.java
+++ Wordle Cheat/src/UserGuess.java
@@ -26,11 +26,26 @@ public class UserGuess {
         System.out.println("Insert '1' for correct position, '2' for correct letter, or '0' for neither: ");
 
         for (int i = 0; i < 5; i++) {
-            int pos = in.nextByte();
+            int pos = in.nextInt();
 
             if (pos >= 0 && pos < 3) {
                 this.guessRes[i] = pos;
             }
         }
+    }
+
+    public boolean hasDuplicateChars() {
+
+        for (int i = 0; i < this.getUserGuess().length(); i++) {
+            for (int j = i + 1; j < this.getUserGuess().length(); j++) {
+                if (Character.toString(this.getUserGuess().charAt(i)).equalsIgnoreCase(Character.toString(this.getUserGuess().charAt(j)))) {
+                    if (this.getGuessRes()[i] != 0 || this.getGuessRes()[j] != 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
